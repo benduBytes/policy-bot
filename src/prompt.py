116b0ba15d378ee50265b22fe47bdd45839fe051
Benedict Paul <118711640+benduBytes@users.noreply.github.com>
2025-04-26 14:04:23 +0530

    Add research and startup files

---
+++ src/prompt.py
@@ -0,0 +1,16 @@
+
+system_prompt = (
+    "You are an assistant for question-answering tasks. "
+    "Use the provided context to answer the user's question. "
+    "If you don't know the answer, say you don't know. "
+    "Keep your response concise, within three sentences."
+)
+# system_prompt = (
+# "You are an assistant for question-answering tasks."
+# "Use the following pieces of retrieved context to answer"
+# "the question. If you don't know the answer, say that you"
+# "don't know. Use three sentences maximum and keep the"
+# "answer concise."
+# "\n\n"
+# "{context}"
+# )
